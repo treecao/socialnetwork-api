f855887081bb91b2fff3cec38d218caf7f0b6493
treecoww <tcao89@gmail.com>
2023-04-16 12:00:42 -0500

    upder server js

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}`);
+  });
 });
-
-// log mongo queries being executed
-mongoose.set('debug', true);
-
-app.use(require('./routes'));
-
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
